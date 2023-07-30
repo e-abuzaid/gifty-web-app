8da4c22d9d5e10719d5646a451b09e63a7474618
unknown <abu.eyaaaad@gmail.com>
2023-07-30 21:27:13 +0300

    modified nextauth api

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,20 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-export const authOptions = {
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/signin",
-    signUp: "/signup",
-  },
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    // ...add more providers here
-  ],
-};
-export default NextAuth(authOptions);
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// export const authOptions = {
+//   session: {
+//     strategy: "jwt",
+//   },
+//   pages: {
+//     signIn: "/signin",
+//     signUp: "/signup",
+//   },
+//   // Configure one or more authentication providers
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID!,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//     }),
+//     // ...add more providers here
+//   ],
+// };
+// export default NextAuth(authOptions);
